abaaf8d5609b90f592cd33a300b97f85166b6957
laplamgor <laplamgor@gmail.com>
2017-10-28 18:31:52 +0800

    Update title display.

--- PRPR/ExReader/Converters/ShortTitleConverter.cs
+++ PRPR/ExReader/Converters/ShortTitleConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PRPR.ExReader.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,65 @@ namespace PRPR.ExReader.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value is string)
+            if (value is ExGallery)
             {
-                var s = (value as string);
-                return GetShortTitle(s);
+                var g = (value as ExGallery);
+                string t;
+                if (!String.IsNullOrEmpty(g.JapaneseTitle))
+                {
+                    t = g.JapaneseTitle;
+                }
+                else
+                {
+                    // Fallback to use english title
+                    t = g.Title;
+                }
+
+                return GetPrefix(g) + GetShortTitle(t);
             }
             else
             {
                 return null;
+            }
+        }
+
+        private static string GetPrefix(ExGallery g)
+        {
+            switch (g.ParsedLanguage)
+            {
+                default:
+                case LanguageType.Other:
+                case LanguageType.NotApplicable:
+                case LanguageType.Japanese:
+                    return "";
+                case LanguageType.English:
+                    return "[Eng]";
+                case LanguageType.Chinese:
+                    return "[中]";
+                case LanguageType.Dutch:
+                    return "[Dutch]";
+                case LanguageType.French:
+                    return "[Fr]";
+                case LanguageType.German:
+                    return "[De]";
+                case LanguageType.Hungarian:
+                    return "[Magyar]";
+                case LanguageType.Italian:
+                    return "[it]";
+                case LanguageType.Korean:
+                    return "[한]";
+                case LanguageType.Polish:
+                    return "[Pol]";
+                case LanguageType.Portuguese:
+                    return "[Port]";
+                case LanguageType.Russian:
+                    return "[русский]";
+                case LanguageType.Spanish:
+                    return "[Es]";
+                case LanguageType.Thai:
+                    return "[ไทย]";
+                case LanguageType.Vietnamese:
+                    return "[Việt]";
             }
         }
 
