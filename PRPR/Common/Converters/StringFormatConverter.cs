374e54db50c28ba494e52a37a6905f4216553373
laplamgor <laplamgor@gmail.com>
2018-01-26 13:48:07 +0800

    Improve UI.

---
+++ PRPR/Common/Converters/StringFormatConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace PRPR.Common.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public string StringFormat { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!String.IsNullOrEmpty(StringFormat))
+            {
+                return String.Format(StringFormat, value);
+            }
+            else
+            {
+                var format = parameter as string;
+                if (!String.IsNullOrEmpty(format))
+                    return String.Format(format, value);
+
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
